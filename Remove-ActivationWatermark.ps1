cdf5ea9f47faf33349b60003cfc293ea67206107
paulpierce34 <33561650+paulpierce34@users.noreply.github.com>
2024-06-14 21:01:18 -0400

    Add files via upload

---
+++ Remove-ActivationWatermark.ps1
@@ -0,0 +1,17 @@
+﻿## Disable Windows Activation Watermark for Windows 10 and 11
+
+$RegKeyPath = "HKCU:\Control Panel\UnsupportedHardwareNotificationCache"
+
+if (Get-ItemProperty -Path $RegKeyPath){
+
+Set-ItemProperty -Path $RegKeyPath -Name "SV2" -Value 0
+write-host -foregroundcolor Cyan "Registry Key has been set. Rebooting system in 10 seconds. Press CTRL+C to abort reboot."
+Start-Sleep 10
+Restart-Computer
+
+}
+else {
+
+write-host "Expected path: $RegKeyPath does not exist. This script will not work."
+
+}
